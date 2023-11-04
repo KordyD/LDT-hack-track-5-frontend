7036a21629622c768c8738ce465d3c3935a5109e
Andrey Burkovetskii <dudya15@yandex.ru>
2023-11-04 19:18:29 +0300

    create greeting page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import '@mantine/core/styles.css';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/Router.tsx';
 import { MantineProvider } from '@mantine/core';
 import { theme } from './theme/Theme.ts';
-import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/Greetings/Greetings.tsx
+++ src/pages/Greetings/Greetings.tsx
@@ -1,18 +1,27 @@
-import {Button, Flex, Text} from "@mantine/core";
-import './Greetings.css'
-import {NavLink} from "react-router-dom";
+import { Button, Flex, Text } from '@mantine/core';
+import { NavLink } from 'react-router-dom';
+import classes from './Greetings.module.css';
 
-const Greetings = () => {
-    return (
-        <Flex className='greetings'>
-            <Text className='greetings__text'>
-                Добро пожаловать!
-                Начни&nbsp;свой&nbsp;путь&nbsp;вместе&nbsp;с&nbsp;нами
-            </Text>
-            <Button className='greetings__button' component={NavLink} to='register'>Регистрация</Button>
-            <Button className='greetings__button' component={NavLink} to='login'>Вход</Button>
-        </Flex>
-    );
+export const Greetings = () => {
+  return (
+    <Flex className={classes.greetings}>
+      <Text className={classes.greetings__text}>
+        Добро пожаловать! Начни&nbsp;свой&nbsp;путь&nbsp;вместе&nbsp;с&nbsp;нами
+      </Text>
+      <Button
+        className={classes.greetings__button}
+        component={NavLink}
+        to='/register'
+      >
+        Регистрация
+      </Button>
+      <Button
+        className={classes.greetings__button}
+        component={NavLink}
+        to='/login'
+      >
+        Вход
+      </Button>
+    </Flex>
+  );
 };
-
-export default Greetings;
