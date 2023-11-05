371722aff3ddc839a761b9557a381e78fca56b05
Raznex <dudya15@yandex.ru>
2023-11-05 17:11:49 +0300

    Добавил шапку в задачи

--- src/pages/Mission/Mission.tsx
+++ src/pages/Mission/Mission.tsx
@@ -1,3 +1,41 @@
+import { Flex, Title, Text, Box, Image } from '@mantine/core';
+import clock from '../../assets/icon/clock-eight.svg';
+import editor from '../../assets/icon/edit.svg';
+import gift from '../../assets/icon/gift.svg';
+
+import classes from './Mission.module.css';
+
 export const Mission = () => {
-  return <div>Account</div>;
+  return (
+    <Flex className={classes.mission}>
+      <Flex className={classes.mission__header}>
+        <Title className={classes.mission__title}>
+          Пройди увлекательный квест
+        </Title>
+        <Text className={classes.mission__text}>
+          Каждый день новые интересные задания для тебя
+        </Text>
+        <Flex gap='16px'>
+          <Flex className={classes.mission__box}>
+            <Image src={clock} className={classes.mission__icon} />
+            <Text className={classes.mission__iconText}>
+              3 месяца увлекательного погружения
+            </Text>
+          </Flex>
+          <Flex className={classes.mission__box}>
+            <Image src={editor} className={classes.mission__icon} />
+            <Text className={classes.mission__iconText}>
+              Тесты после каждого блока заданий
+            </Text>
+          </Flex>
+          <Flex className={classes.mission__box}>
+            <Image src={gift} className={classes.mission__icon} />
+            <Text className={classes.mission__iconText}>
+              Призы за успешное выполнение
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -32,7 +32,7 @@ export const router = createBrowserRouter([
         element: <Team />,
       },
       {
-        path: 'account',
+        path: 'mission',
         element: <Mission />,
       },
       {
