0b3489eaef022aba5c5665f3d14fffb6f9dcb8b3
Raznex <dudya15@yandex.ru>
2023-11-05 22:27:08 +0300

    Добавил страницу задач и попап

--- src/pages/Mission/Mission.tsx
+++ src/pages/Mission/Mission.tsx
@@ -1,13 +1,14 @@
-import { Flex, Title, Text, Box, Image } from '@mantine/core';
+import { Flex, Title, Text, Image, Button } from '@mantine/core';
 import clock from '../../assets/icon/clock-eight.svg';
 import editor from '../../assets/icon/edit.svg';
 import gift from '../../assets/icon/gift.svg';
 
 import classes from './Mission.module.css';
+import { MissionAccordion } from '../../modules/MissionAccordion/MissionAccordion';
 
 export const Mission = () => {
   return (
-    <Flex className={classes.mission}>
+    <Flex className={classes.mission} bg='#F8F8F8'>
       <Flex className={classes.mission__header}>
         <Title className={classes.mission__title}>
           Пройди увлекательный квест
@@ -17,25 +18,26 @@ export const Mission = () => {
         </Text>
         <Flex gap='16px'>
           <Flex className={classes.mission__box}>
-            <Image src={clock} className={classes.mission__icon} />
+            <Image src={clock} w='50px' h='50px' />
             <Text className={classes.mission__iconText}>
               3 месяца увлекательного погружения
             </Text>
           </Flex>
           <Flex className={classes.mission__box}>
-            <Image src={editor} className={classes.mission__icon} />
+            <Image src={editor} w='50px' h='50px' />
             <Text className={classes.mission__iconText}>
               Тесты после каждого блока заданий
             </Text>
           </Flex>
           <Flex className={classes.mission__box}>
-            <Image src={gift} className={classes.mission__icon} />
+            <Image src={gift} w='50px' h='50px' />
             <Text className={classes.mission__iconText}>
               Призы за успешное выполнение
             </Text>
           </Flex>
         </Flex>
       </Flex>
+      <MissionAccordion />
     </Flex>
   );
 };
