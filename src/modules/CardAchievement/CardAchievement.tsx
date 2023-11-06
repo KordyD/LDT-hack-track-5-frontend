0eb369efa5517a3a62c4923923ddf41ddd0d0bf0
KordyD <84672403+KordyD@users.noreply.github.com>
2023-11-06 17:49:35 +0300

    Add achievements to account page

---
+++ src/modules/CardAchievement/CardAchievement.tsx
@@ -0,0 +1,33 @@
+import { Anchor, Box, Button, Card, SimpleGrid } from '@mantine/core';
+import { Achievement } from '../../components/Achievement/Achievement';
+import { achievements } from '../../helpers/Achievements';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import classes from './CardAchievement.module.css';
+
+export const CardAchievement = () => {
+  return (
+    <Card className={classes.card}>
+      <SimpleGrid cols={5} className={classes.group}>
+        {achievements.map((item) => (
+          <Achievement
+            key={item.id}
+            title={item.title}
+            text={item.text}
+            image={item.image}
+            completed={item.completed}
+          />
+        ))}
+      </SimpleGrid>
+      <Box className={classes.wrapperButton}>
+        <Button
+          component={Anchor}
+          className={classes.button}
+          variant='transparent'
+          leftSection={<AiOutlineArrowRight />}
+        >
+          Смотреть всё
+        </Button>
+      </Box>
+    </Card>
+  );
+};
