90c4a39d5221fde55df3f6974c0d5fe6b138d3de
KordyD <84672403+KordyD@users.noreply.github.com>
2023-11-08 17:09:13 +0300

    Update article page for admin

--- src/pages/Article/Article.tsx
+++ src/pages/Article/Article.tsx
@@ -1,10 +1,21 @@
-import { ActionIcon, Box, Button, Modal, Text, Title } from '@mantine/core';
+import {
+  ActionIcon,
+  Box,
+  Button,
+  Modal,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
 import { BiSolidBookmark } from 'react-icons/bi';
 import { useSelector } from 'react-redux';
 import { useLoaderData } from 'react-router-dom';
 import { BsTrash3 } from 'react-icons/bs';
 import { RootState } from '../../store';
 import { useDisclosure } from '@mantine/hooks';
+import classes from './Article.module.css';
+import { BackButton } from '../../components/BackButton/BackButton';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 interface loaderData {
   title: string;
@@ -19,19 +30,30 @@ export const Article = () => {
   return (
     <>
       <Box pos='relative'>
-        <Box display={isAuth ? 'block' : 'none'}>
-          <Button onClick={open}>Редактировать</Button>
-          <ActionIcon>
-            <BsTrash3 />
+        <BackButton leftSection={<AiOutlineArrowLeft />} variant='transparent'>
+          Назад
+        </BackButton>
+        <Box className={classes.box}>
+          <Button display={isAuth ? 'block' : 'none'} variant='white'>
+            Редактировать
+          </Button>
+          <Box display={isAuth ? 'block' : 'none'}>
+            <ActionIcon variant='transparent' color='red' onClick={open}>
+              <BsTrash3 />
+            </ActionIcon>
+            <Modal radius='xl' opened={opened} onClose={close}>
+              <Stack align='center'>
+                <Text className={classes.modalText}>
+                  Вы уверены, что хотите удалить эту статью?
+                </Text>
+                <Button className={classes.modalButton}>Удалить</Button>
+              </Stack>
+            </Modal>
+          </Box>
+          <ActionIcon display={isAuth ? 'none' : 'block'} variant='transparent'>
+            <BiSolidBookmark style={{ width: 40, height: 40 }} />
           </ActionIcon>
-          <Modal opened={opened} onClose={close}>
-            <Text>Вы уверены, что хотите удалить эту статью?</Text>
-            <Button>Удалить</Button>
-          </Modal>
         </Box>
-        <ActionIcon pos='absolute' variant='transparent' right='0'>
-          <BiSolidBookmark style={{ width: 40, height: 40 }} />
-        </ActionIcon>
         <Title>{title}</Title>
         <Text>{text}</Text>
       </Box>
