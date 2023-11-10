16ec819657a3286161c809cb40c4e0a38f709c43
Andrey Burkovetskii <dudya15@yandex.ru>
2023-11-10 20:42:05 +0300

    Добавление видео

--- src/components/Lecture/LectureVideo.tsx
+++ src/components/Lecture/LectureVideo.tsx
@@ -1,15 +1,19 @@
-import { Flex, AspectRatio, Text } from '@mantine/core';
+import { Flex, AspectRatio, Text, Image, Button } from '@mantine/core';
 import {
+  Image40,
   TextForInput,
   TextMiddle,
   TextSmall,
 } from '../../theme/AdaptiveConts.ts';
 import { VideoProps } from '../../modules/VideoLecture/VideoLecture';
+import Trash from '../../assets/images/TrashBin.png';
 import classes from './LectureVideo.module.css';
 
 interface LectureVideo {
   video: VideoProps;
 }
+
+const role = 'ADMIN';
 
 export const LectureVideo = ({ video }: LectureVideo) => {
   return (
@@ -24,26 +28,44 @@ export const LectureVideo = ({ video }: LectureVideo) => {
           allowFullScreen
         />
       </AspectRatio>
-      <Flex direction='column' m='30px auto 40px 22px'>
-        <Flex align='center' gap='13px' mb='8px'>
-          <Text
-            truncate='end'
-            className={classes.lecturevideo__title}
-            fz={TextMiddle}
-          >
-            {video.title}
-          </Text>
-          <Text
-            truncate='end'
-            className={classes.lecturevideo__description}
-            fz={TextSmall}
-          >
-            {video.description}
+      <Flex
+        direction='row'
+        m='30px auto 40px 22px'
+        justify={'space-between'}
+        w='90%'
+      >
+        <Flex direction='column'>
+          <Flex align='center' gap='13px' mb='8px'>
+            <Text
+              truncate='end'
+              className={classes.lecturevideo__title}
+              fz={TextMiddle}
+            >
+              {video.title}
+            </Text>
+            <Text
+              truncate='end'
+              className={classes.lecturevideo__description}
+              fz={TextSmall}
+            >
+              {video.description}
+            </Text>
+          </Flex>
+          <Text className={classes.lecturevideo__department} fz={TextForInput}>
+            {video.department}
           </Text>
         </Flex>
-        <Text className={classes.lecturevideo__department} fz={TextForInput}>
-          {video.department}
-        </Text>
+        {role === 'ADMIN' ? (
+          <Button
+            variant='white'
+            rightSection={<Image src={Trash} w={Image40} h={Image40} />}
+            classNames={{
+              root: classes.lecturevideo__button,
+            }}
+          ></Button>
+        ) : (
+          ''
+        )}
       </Flex>
     </Flex>
   );
--- src/modules/VideoLecture/VideoLecture.tsx
+++ src/modules/VideoLecture/VideoLecture.tsx
@@ -5,11 +5,19 @@ import {
   Flex,
   SimpleGrid,
   Card,
+  Textarea,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { AiOutlinePaperClip } from 'react-icons/ai';
+import { Modal, Select } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { Simulate } from 'react-dom/test-utils';
 import { LectureVideo } from '../../components/Lecture/LectureVideo';
 import { BackButton } from '../../components/BackButton/BackButton.tsx';
+import { TextForInput } from '../../theme/AdaptiveConts.ts';
+import { addVideos } from '../../API/admin';
 import classes from './VideoLecture.module.css';
+import error = Simulate.error;
 
 export interface VideoProps {
   id: number;
@@ -18,6 +26,15 @@ export interface VideoProps {
   description: string;
   department: string;
 }
+
+// const transformHTTP = (src) => {
+//   const newHTTP = src.split('live').join('embed');
+//   return newHTTP;
+// };
+//
+// console.log(
+//   transformHTTP('https://www.youtube.com/live/jfKfPfyJRdk?si=kJdDHKPBBEgw8fTm')
+// );
 
 const video = [
   {
@@ -49,51 +66,157 @@ const video = [
     department: 'IT отдел',
   },
 ];
+const role = 'ADMIN';
 
 export const VideoLecture = () => {
+  const [opened, { open, close }] = useDisclosure(false);
+  const formVideo = useForm({
+    initialValues: {
+      date: '',
+      name: '',
+    },
+
+    validate: {
+      date: (value) => (/.+/.test(value) ? null : 'Нужно выбрать дату'),
+      name: (value) =>
+        /.+/.test(value)
+          ? null
+          : 'Название должно состоять хотя бы из 1 символа',
+    },
+  });
+
+  const closePopup = () => {
+    close();
+    form.reset();
+  };
+
   const form = useForm({
     initialValues: {
       videoName: '',
     },
   });
+
+  const handleSubmit = (data) => {
+    addVideos(data)
+      .then()
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <Flex direction='column' align='center' bg='#FFF' mt='30px'>
-      <Flex w='100%' justify='space-between' align='flex-end'>
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+    <>
+      <Flex direction='column' align='center' bg='#FFF' mt='30px'>
+        <Flex w='100%' justify='space-between' align='flex-end'>
+          <form
+            onSubmit={form.onSubmit((values) => {
+              handleSubmit(values);
+              closePopup();
+            })}
+          >
+            <TextInput
+              w='100%'
+              classNames={{ input: classes.videolecture__input }}
+              placeholder='Введите ключевые слова'
+              {...form.getInputProps('videoName')}
+            />
+
+            <Group justify='flex-start' mt='md'>
+              <Button
+                className={`${classes.videolecture__button} ${classes.videolecture__buttonSearch}`}
+                type='submit'
+              >
+                Найти
+              </Button>
+              <Button className={classes.videolecture__button} type='button'>
+                Очистить
+              </Button>
+            </Group>
+          </form>
+          <BackButton>Назад</BackButton>
+        </Flex>
+        {role === 'ADMIN' ? (
+          <Flex w='100%' mt='20px'>
+            <Button
+              variant='white'
+              color='#5277F6'
+              rightSection={<AiOutlinePaperClip />}
+              onClick={open}
+            >
+              Добавить видео
+            </Button>
+          </Flex>
+        ) : (
+          ''
+        )}
+        <SimpleGrid
+          cols={{ lg: 3, md: 2, sm: 2, base: 1 }}
+          justify='flex-start'
+          align='flex-start'
+          gutter='30px'
+          m='10px 0 140px'
+        >
+          {video.map((item) => (
+            <Card key={item.id}>
+              <LectureVideo video={item} />
+            </Card>
+          ))}
+        </SimpleGrid>
+      </Flex>
+      <Modal
+        radius='32px'
+        fz={TextForInput}
+        opened={opened}
+        onClose={() => {
+          closePopup();
+        }}
+        centered
+        title='Добавить видео'
+        overlayProps={{
+          backgroundOpacity: 0.6,
+          blur: 3,
+        }}
+      >
+        <form
+          className={classes.videolecture__form}
+          onSubmit={formVideo.onSubmit((values) => {
+            console.log(values);
+            closePopup();
+          })}
+        >
           <TextInput
             w='100%'
+            fz={TextForInput}
             classNames={{ input: classes.videolecture__input }}
-            placeholder='Введите ключевые слова'
-            {...form.getInputProps('videoName')}
+            placeholder='Название'
+            {...formVideo.getInputProps('name')}
           />
-
-          <Group justify='flex-start' mt='md'>
-            <Button
-              className={`${classes.videolecture__button} ${classes.videolecture__buttonSearch}`}
-              type='submit'
-            >
-              Найти
-            </Button>
-            <Button className={classes.videolecture__button} type='button'>
-              Очистить
-            </Button>
-          </Group>
+          <Textarea
+            w='100%'
+            fz={TextForInput}
+            classNames={{ input: classes.videolecture__input }}
+            placeholder='Описание'
+            {...formVideo.getInputProps('description')}
+          />
+          <TextInput
+            w='100%'
+            fz={TextForInput}
+            classNames={{ input: classes.videolecture__input }}
+            placeholder='Ссылка'
+            {...formVideo.getInputProps('url')}
+          />
+          <Select
+            withCheckIcon={false}
+            w='100%'
+            placeholder='Должность'
+            fz={TextForInput}
+            classNames={{ input: classes.videolecture__input }}
+            data={['', 'React', 'Angular', 'Vue', 'Svelte']}
+            {...formVideo.getInputProps('postName')}
+          />
+          <Button w='55%' type='submit' fz={TextForInput}>
+            Создать этап
+          </Button>
         </form>
-        <BackButton>Назад</BackButton>
-      </Flex>
-      <SimpleGrid
-        cols={{ lg: 3, md: 2, sm: 2, base: 1 }}
-        justify='flex-start'
-        align='flex-start'
-        gutter='30px'
-        m='70px 0 140px'
-      >
-        {video.map((item) => (
-          <Card key={item.id}>
-            <LectureVideo video={item} />
-          </Card>
-        ))}
-      </SimpleGrid>
-    </Flex>
+      </Modal>
+    </>
   );
 };
