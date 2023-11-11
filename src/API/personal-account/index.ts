b7b545062ca9e7b0a7885ffdf3e577f2bd161b28
KordyD <84672403+KordyD@users.noreply.github.com>
2023-11-11 20:47:31 +0300

    Fix

--- src/API/personal-account/index.ts
+++ src/API/personal-account/index.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import { URL } from '..';
 import { headers } from '../';
+import { departmentAnalytitcs } from '../curator/interfaces';
 import { EmployeeData, updatePersonal } from './interfaces';
 
 export const getPersonalData = async () => {
@@ -15,6 +16,14 @@ export const updatePersonalData = async (data: updatePersonal) => {
   const response: AxiosResponse<EmployeeData> = await axios.put(
     URL + '/personal-account',
     data,
+    { headers: { ...headers } }
+  );
+  return response.data;
+};
+
+export const getPersonalAnalytics = async () => {
+  const response: AxiosResponse<departmentAnalytitcs[]> = await axios.get(
+    URL + '/personal-account/analytic',
     { headers: { ...headers } }
   );
   return response.data;
